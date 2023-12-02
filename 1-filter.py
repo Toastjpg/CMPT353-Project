ea865db6f9972da6185380e9f123d86743af4f59
Johnny Mai <jma184@sfu.ca>
2023-12-01 23:08:59 -0800

    edit static path for filter

--- 1-filter.py
+++ 1-filter.py
@@ -81,8 +81,8 @@ def main(input, output):
     posts.write.json(output, mode='overwrite', compression='gzip')    
 
 if __name__ == '__main__':
-    inputs = sys.argv[1]
-    output = sys.argv[2]
+    inputs = '/reddit-subset/submissions/'
+    output = '/submissions-filtered/'
     spark = SparkSession.builder.appName('filter reddit-subset').getOrCreate()
     assert spark.version >= '3.4' # make sure we have Spark 3.4+
     spark.sparkContext.setLogLevel('WARN')
